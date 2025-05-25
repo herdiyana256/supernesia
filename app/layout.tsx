7fe3653926aff03501537c3a24f8392c94b65dab
herdiyana256 <herdiyanadam256@gmail.com>
2025-05-25 21:41:28 +0700

    rechange favicon supernesia

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,16 +10,25 @@ const anton = Anton({
 })
 
 export const metadata: Metadata = {
-  title: "Supernesia - Transformasikan Bisnismu Jadi Lebih Super",
-  description: "Jasa pembuatan Website & Aplikasi untuk UMKM atau Bisnis yang mau naik kelas.",
-  keywords: "jasa pembuatan website, aplikasi mobile, pembuatan aplikasi, digital transformation, UMKM, solusi digital, web development Indonesia, jasa pembuatan aplikasi, digital marketing UMKM, aplikasi custom untuk bisnis, solusi teknologi untuk bisnis, pembuatan website profesional",
+  title: "Supernesia - Jasa Pembuatan Website & Aplikasi, Software untuk UMKM dan Bisnis",
+  description:
+    "Supernesia menyediakan jasa pembuatan website profesional, aplikasi mobile, dan solusi digital custom untuk UMKM dan bisnis yang ingin naik kelas. Transformasi digital dengan layanan terpercaya di Indonesia.",
+  keywords:
+    "jasa pembuatan website, jasa pembuatan aplikasi, aplikasi mobile, pengembangan aplikasi custom, solusi digital untuk UMKM, digital marketing UMKM, pembuatan website profesional, transformasi digital bisnis, pengembangan software bisnis, layanan IT untuk UMKM",
   generator: "Next.js",
-  author: "Supernesia Team",
+  author: "Supernesia Creative Technology",
   robots: "index, follow",
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+            { url: "/favicon.ico", sizes: "16x16", type: "image/ico" },
+      { url: "/favicon_16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon_32.png", sizes: "32x32", type: "image/png" }
+    ],
+    shortcut: "/favicon_32.png",
+    apple: "/apple-touch-icon.png",
   },
-}
+};
+
 
 export default function RootLayout({
   children,
